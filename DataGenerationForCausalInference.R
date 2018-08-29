fc750c011d50811d31ede19a911fcc27469e7f35
DanielAubin <DanielAubin@users.noreply.github.com>
2018-08-29 14:48:15 +0200

    Reformatted code

--- DataGenerationForCausalInference.R
+++ DataGenerationForCausalInference.R
@@ -10,48 +10,69 @@
 # var = Size of the Variance (Noise-level)
 
 #Required Packages
-library(clusterGeneration)
-library(mvtnorm)
+if(!require("clusterGeneration")) install.packages("clusterGeneration"); library("clusterGeneration")
+if(!require("mvtnorm")) install.packages("mvtnorm"); library("mvtnorm")
+
 
 
 datagen <- function(N,k,random_d,theta,var) {
   
-  N=N 
-  k=k 
-  b=1/(1:k)
+  N = N
+  k = k
+  b = 1 / (1:k)
   # = Generate covariance matrix of z = #
-  sigma=genPositiveDefMat(k,"unifcorrmat")$Sigma
-  sigma=cov2cor(sigma)
+  sigma <- genPositiveDefMat(k, "unifcorrmat")$Sigma
+  sigma <- cov2cor(sigma)
   
   
-  z=rmvnorm(N,sigma=sigma) # = Generate z = #
+  z <- rmvnorm(N, sigma = sigma) # = Generate z = #
   
   
   ### Options for D (m_(X))
-  if(random_d==T) {d <- rep(c(0,1),length.out=N)} else {d_prop <- pnorm(z%*%b) # D is dependent on Z 
-  d <- as.numeric(rbinom(N,prob=d_prop, size=1))}
+  if (random_d == T) {
+    d <- rep(c(0, 1), length.out = N)
+  } else {
+    d_prop <- pnorm(z %*% b) # D is dependent on Z
+    d <- as.numeric(rbinom(N, prob = d_prop, size = 1))
+  }
   
   
   ### Options for theta
-  if (theta=="con") {theta_s <- as.vector(sin(z%*%b)^2) 
-  theta <- (theta_s-min(theta_s))*(0.3-0.1)/(max(theta_s)-min(theta_s))+0.1} else if(theta=="binary") {theta_low <- rbinom(N,pnorm((z[,6]*(z[,1]%*%t(z[,5]))*z[,2])^2),size=1)
-  theta <- ifelse(theta_low==1,0.3,0.1)} else if(theta=="big"){theta_big <- rbinom(N,pnorm((z[,6]*(z[,1]%*%t(z[,5]))*z[,2])^2),size=1)
-  theta <- ifelse(theta_big==1,1,0.4)}  else {theta==theta}
+  if (theta == "con") {
+    theta_s <- as.vector(sin(z %*% b) ^ 2)
+    theta <-
+      (theta_s - min(theta_s)) * (0.3 - 0.1) / (max(theta_s) - min(theta_s)) +
+      0.1
+  } else if (theta == "binary") {
+    theta_low <-
+      rbinom(N, pnorm((z[, 6] * (z[, 1] %*% t(
+        z[, 5]
+      )) * z[, 2]) ^ 2), size = 1)
+    theta <-
+      ifelse(theta_low == 1, 0.3, 0.1)
+  } else if (theta == "big") {
+    theta_big <-
+      rbinom(N, pnorm((z[, 6] * (z[, 1] %*% t(
+        z[, 5]
+      )) * z[, 2]) ^ 2), size = 1)
+    theta <- ifelse(theta_big == 1, 1, 0.4)
+  }  else {
+    theta == theta
+  }
   
-
-  g=as.vector(cos(z%*%b)^2)
   
-  y=theta*d+g+rnorm(N,var)
+  g <- as.vector(cos(z %*% b) ^ 2)
+  
+  y <- theta * d + g + rnorm(N, var)
   
   data <- as.data.frame(y)
-  data <- cbind(data,theta,d,z)
-  colnames(data) <- c("y","theta","d",c(paste0("V", 1:k)))
+  data <- cbind(data, theta, d, z)
+  colnames(data) <- c("y", "theta", "d", c(paste0("V", 1:k)))
   
   return(data)
-} 
+}
 
 
-### Example 
-test <- datagen(N=5000,k=20,random_d=F,theta="binary",var=0.25)
+### Example
+test <- datagen(N = 5000, k = 20, random_d = F, theta = "binary", var = 0.25)
 summary(test)
-
